3bcbe2e64cbd037f766e799e36a93e12c239b971
ruben <rubendasilva@me.com>
2022-04-26 22:38:56 -0700

    end of night

--- routes/tickets.js
+++ routes/tickets.js
@@ -3,7 +3,8 @@ const router = express.Router();
 
 const ticketsCtrl = require('../controllers/tickets');
 
-router.get('/tickets/new', ticketsCtrl.new);
+router.get('/new', ticketsCtrl.new);
+router.get('/:id', ticketsCtrl.index);
 router.post('/tickets', ticketsCtrl.create);
 router.post('/flights/:id/tickets', ticketsCtrl.addToTickets);
 
